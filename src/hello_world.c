9349341f461c3512f64f110ed1760d1c1ec3e20b
Cameron A. Craig <camieac@gmail.com>
2019-08-19 17:19:12 +0100

    Add C hello world.

---
+++ src/hello_world.c
@@ -0,0 +1,12 @@
+/**
+ * @file hello_world.c
+ * @brief Hello world in C
+ * @copyright Cameron A. Craig 2019
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("Hello World!");
+	return EXIT_SUCCESS;
+}
