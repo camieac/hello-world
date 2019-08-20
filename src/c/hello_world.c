a2d7e517c45c885548cab0cf963f317b8d4bb7ee
Cameron A. Craig <camieac@gmail.com>
2019-08-20 17:11:32 +0100

    Hello world in C.

---
+++ src/c/hello_world.c
@@ -0,0 +1,13 @@
+/**
+ * @file hello_world.c
+ * @brief Hello world in C
+ * @copyright Cameron A. Craig 2019
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	printf("Hello World!\r\n");
+	return EXIT_SUCCESS;
+}
